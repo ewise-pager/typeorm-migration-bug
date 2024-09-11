5376d08e187a36a580ca49107ab26caba29a3d21
Evan Wise <Evan_Wise@onlifehealth.com>
2024-09-11 16:34:09 -0500

    feat(main): initial commit, docker compose for db, add entity, generate initial migration

---
+++ data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5434,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'mydb',
+  entities: [__dirname + '/entities/*.ts'],
+  synchronize: false,
+  logging: false,
+  migrations: [__dirname + '/migrations/*.ts'],
+});
+
+export default AppDataSource;
